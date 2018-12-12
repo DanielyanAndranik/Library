3fbce89a55799c584c6aa503862defd42b898f6a
Andranik <danielyandranik@gmail.com>
2018-12-12 18:57:31 +0400

    regular commit

--- WebApp/ClientApp/src/app/app.module.ts
+++ WebApp/ClientApp/src/app/app.module.ts
@@ -10,6 +10,7 @@ import { LoginComponent } from './login/login.component';
 import { MainComponent } from './main/main.component';
 import { FooterComponent } from './footer/footer.component';
 import { SignupComponent } from './signup/signup.component';
+import { LibrarianDashboardComponent } from './librarian-dashboard/librarian-dashboard.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -17,7 +18,8 @@ import { SignupComponent } from './signup/signup.component';
     LoginComponent,
     MainComponent,
     FooterComponent,
-    SignupComponent
+    SignupComponent,
+    LibrarianDashboardComponent
   ],
   imports: [
     BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
@@ -26,7 +28,8 @@ import { SignupComponent } from './signup/signup.component';
 	  RouterModule.forRoot([
 		  { path: '', component: MainComponent, pathMatch: 'full' },
 		  { path: 'login', component: LoginComponent, pathMatch: 'full' },
-		  { path: 'signup', component: SignupComponent, pathMatch: 'full' }
+		  { path: 'signup', component: SignupComponent, pathMatch: 'full' },
+		  { path: 'lib_dashboard', component: LibrarianDashboardComponent, pathMatch: 'full' }
     ])
   ],
   providers: [],
---
+++ WebApp/ClientApp/src/app/librarian-dashboard/librarian-dashboard.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LibrarianDashboardComponent } from './librarian-dashboard.component';
+
+describe('LibrarianDashboardComponent', () => {
+  let component: LibrarianDashboardComponent;
+  let fixture: ComponentFixture<LibrarianDashboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ LibrarianDashboardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LibrarianDashboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
