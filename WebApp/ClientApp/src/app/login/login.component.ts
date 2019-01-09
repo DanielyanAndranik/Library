2366ed4e59d536290903a939e023c3d6f7b5b622
Andranik <danielyandranik@gmail.com>
2019-01-09 18:47:45 +0400

    Changed signup component

--- WebApp/ClientApp/src/app/login/login.component.ts
+++ WebApp/ClientApp/src/app/login/login.component.ts
@@ -6,10 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
+	username: string;
+	password: string;
   constructor() { }
 
   ngOnInit() {
   }
+
+	login() {
+		console.log(this.username);
+		console.log(this.password);
+		
+		
+	}
 
 }
--- WebApp/ClientApp/src/app/signup/signup.component.ts
+++ WebApp/ClientApp/src/app/signup/signup.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, state } from '@angular/core';
 
 @Component({
   selector: 'app-signup',
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./signup.component.css']
 })
 export class SignupComponent implements OnInit {
-
+	state1: boolean;
+	state2: boolean;
+	state3: boolean;
   constructor() { }
 
-  ngOnInit() {
-  }
-
+	ngOnInit() {
+		this.state1 = true;
+		this.state2 = false;
+		this.state3 = false;
+	}
+	toState1() {
+		console.log("45");
+		this.state1 = true;
+		this.state2 = false;
+	}
+	toState2() {
+		console.log("45");
+		this.state1 = false;
+		this.state2 = true;
+	}
+	toState3() {
+		console.log("45");
+		this.state3 = true;
+		this.state2 = false;
+	}
 }
