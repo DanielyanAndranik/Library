e7ed9a43f4cc03e15c7583142c2a1a10f9278da0
Andranik <danielyandranik@gmail.com>
2019-01-11 18:46:05 +0400

    Worked on validation

--- WebApp/ClientApp/src/app/signup/signup.component.ts
+++ WebApp/ClientApp/src/app/signup/signup.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, ReactiveFormsModule, Validators } from '@angular/forms';
 
+declare var init: Function;
+
 @Component({
 	selector: 'app-signup',
 	templateUrl: './signup.component.html',
@@ -10,22 +12,29 @@ export class SignupComponent implements OnInit {
 	state1: boolean;
 	state2: boolean;
 	state3: boolean;
-	constructor() { }
+	constructor() {}
 
 	registerForm: FormGroup;
 
 	ngOnInit() {
+		init();
 		this.registerForm = new FormGroup({
 			name: new FormGroup({
-				firstName: new FormControl(),
-				middleName: new FormControl(),
-				lastName: new FormControl(),
+				firstName: new FormControl('', Validators.required),
+				middleName: new FormControl('', Validators.required),
+				lastName: new FormControl('', Validators.required),
 			}),
 			birthDate: new FormControl(),
 			phone: new FormControl(),
-			email: new FormControl(),
+			email: new FormControl('', [
+				Validators.required,
+				Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+			]),
 			username: new FormControl(),
-			password: new FormControl(),
+			password: new FormControl('', [
+				Validators.required,
+				Validators.minLength(8)
+			]),
 			repeatPassword: new FormControl()
 		});
 		this.state1 = true;
@@ -47,5 +56,13 @@ export class SignupComponent implements OnInit {
 	nextTo3() {
 		this.state3 = true;
 		this.state2 = false;
+	}
+
+	isFirstStepValid() {
+		return this.registerForm.controls.name.valid;
+	}
+
+	isSecondStepValid() {
+		return this.registerForm.controls.birthDate.valid && this.registerForm.controls.phone.valid && this.registerForm.controls.email.valid;
 	}
 }
