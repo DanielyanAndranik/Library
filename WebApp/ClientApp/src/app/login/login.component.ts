afe8407bd9f93a84531589100b710e948dd07917
Andranik <danielyandranik@gmail.com>
2019-01-18 18:45:59 +0400

    Adding authentication

--- WebApp/ClientApp/src/app/login/login.component.ts
+++ WebApp/ClientApp/src/app/login/login.component.ts
@@ -1,23 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AuthService } from '../services/AuthService/auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-	username: string;
-	password: string;
-  constructor() { }
 
-  ngOnInit() {
-  }
+	loginForm: FormGroup;
 
-	login() {
-		console.log(this.username);
-		console.log(this.password);
-		
-		
+	constructor(private auth: AuthService) { }
+
+	ngOnInit() {
+		this.loginForm = new FormGroup({
+			username: new FormControl('', Validators.required),
+			password: new FormControl('', Validators.required)
+		});
 	}
 
+	canLogin() {
+		return this.loginForm.controls.username.valid && this.loginForm.controls.password.valid;
+	}
+
+	login() {
+
+		var username = this.loginForm.value.username;
+		var password = this.loginForm.value.password;
+		this.auth.login(username, password).subscribe(
+			res => { localStorage.setItem('token', res.token), },
+			err => { }
+		);
+	}
 }
